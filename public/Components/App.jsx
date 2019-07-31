96fc3ab2feade69227bbc394df1f418d233a9687
r3d-fan6 <dtetheridge@gmail.com>
2019-07-31 22:02:30 +0100

    Login components.

--- public/Components/App.jsx
+++ public/Components/App.jsx
@@ -3,11 +3,13 @@ import React, { Fragment } from "react";
 
 // Components.
 import Temp from "./Temp";
+import Login from "./Login";
 
 export default function App() {
     return(
         <Fragment>
             <Temp/>
+            <Login/>
         </Fragment>
     );
 }
---
+++ public/Components/Login.jsx
@@ -0,0 +1,70 @@
+// Modules.
+import React from "react";
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            // Password information and check booleans.
+            data: [{
+                "user1": "pass1",
+                "user2": "pass2",
+                "user3": "pass3",
+                "user4": "pass4"
+            }],
+            passwords: [
+                
+            ],
+            username: null,
+            password: null,
+            usernameCheck: false,
+            passwordCheck: false
+        }
+
+        this.authenticate = this.authenticate.bind(this);
+        this.updateCredentials = this.updateCredentials.bind(this);
+    }
+
+    // Authentication when logging in.
+    authenticate(e) {
+        e.preventDefault();
+
+        if (this.state.username < 5 || !(this.state.username in this.state.data[0])) {
+            this.setState({ "usernameCheck": false });
+            document.getElementById("loginError").style.display = "block";
+        } else {
+            this.setState({ "usernameCheck": true });
+            document.getElementById("loginError").style.display = "none";
+        }
+
+        if (this.state.password < 5 || this.state.password != this.state.data[0][this.state.username]) {
+            this.setState({ "passwordCheck": false });
+            document.getElementById("loginError").style.display = "block";
+        } else {
+            this.setState({ "passwordCheck": true });
+            document.getElementById("loginError").style.display = "none";
+        }
+
+        if (this.state.usernameCheck && this.state.passwordCheck) {
+            location.href = "#";
+        }
+    }
+
+    // Updating the credentials.
+    updateCredentials(e) {
+        this.setState({ [e.target.id]: e.target.value });
+    }
+
+    render() {
+        return(
+            <div>
+                <p id="loginError" style={{display: "none"}}>There was a problem with logging in... Please try again.</p>
+                <form onSubmit={this.authenticate}>
+                    <input id="username" placeholder="Username" onChange={this.updateCredentials}></input>
+                    <input id="password" placeholder="Password" type="password" onChange={this.updateCredentials}></input>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
