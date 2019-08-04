e211a9e99a60fa2884ff0c26bbe4801303f79435
rethavanderwalt <42917205+rethavanderwalt@users.noreply.github.com>
2019-08-05 09:13:38 +1200

    Toolbar.jsx added
    Plus imports for nav in App.jsx

--- public/Components/App.jsx
+++ public/Components/App.jsx
@@ -4,6 +4,10 @@ import React, { Fragment } from "react";
 // Components.
 import Home from "./Home";
 import Footer from "./Footer";
+// Nav Components
+import Toolbar from "../Components/Toolbar";
+import SideDrawer from "../Javascript/SideDrawer/SideDrawer";
+import Backdrop from "../Javascript/SideDrawer/Backdrop";
 
 export default function App() {
     return(
---
+++ public/Components/Toolbar.jsx
@@ -0,0 +1,38 @@
+// Modules.
+import React from 'react';
+import '../Javascript/SideDrawer/DrawerToggleButton';
+import DrawerToggleButton from '../Javascript/SideDrawer/DrawerToggleButton';
+    
+export default class Toolbar extends React.Component {
+    render() {
+        return(
+            <header className='toolbar' style={{width: '100%', position: 'fixed', background: 'grey', height: '56px', top: '0', left: '0'}}>
+                <nav className='toolbar__navigation' style={{display: 'flex', height: '100%', alignItems: 'center', padding: '0 1rem'}}>
+                    
+                    <div>
+                        <DrawerToggleButton click={this.props.drawerClickHandler}/>
+                    </div>
+
+                    <div className='toolbar__logo' style={{marginLeft: '1rem'}}>
+                        <a href="/" style={{color: 'white', textDecoration: 'none', fontSize: '1.5rem'}}>The Logo</a></div>
+                    
+                    <div className='spacer' style={{flex: '1'}}/>
+
+                    <div className='toolbar__navigation-items'>
+                        <ul style={{listStyle: 'none', margin: '0', padding: '0', display: 'flex'}}>
+                            <li style={{padding: '0 0.5rem'}}>
+                                <a href="/" style={{color: 'white', textDecoration: 'none'}}>Home</a></li>
+                            <li style={{padding: '0 0.5rem'}}>
+                                <a href="/" style={{color: 'white', textDecoration: 'none'}}>About</a></li>
+                            <li style={{padding: '0 0.5rem'}}>
+                                <a href="/" style={{color: 'white', textDecoration: 'none'}}>Solutions</a></li>
+                            <li style={{padding: '0 0.5rem'}}>
+                                <a href="/" style={{color: 'white', textDecoration: 'none'}}>Developers</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        );
+    }
+}
+
