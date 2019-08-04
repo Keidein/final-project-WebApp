46fdef2953d9820a5a2f71aa658b378040822406
rethavanderwalt <42917205+rethavanderwalt@users.noreply.github.com>
2019-08-05 09:25:04 +1200

    Toolbar as App_withNav
    draft

---
+++ public/Components/App copy.jsx
@@ -0,0 +1,49 @@
+// Modules.
+import React, { Component } from "react";
+
+// Components.
+import Toolbar from "../Components/Toolbar";
+import SideDrawer from "../Javascript/SideDrawer/SideDrawer";
+import Backdrop from "../Javascript/SideDrawer/Backdrop";
+
+class App extends Component {
+    // NAVIGATION
+    state = {
+        sideDrawerOpen: false
+    };
+    drawerToggleClickHandler = () => {
+        this.setState((prevState) => {
+            return {sideDrawerOpen: !prevState.sideDrawerOpen};
+        });
+    };
+    backdropClickHandler = () => {
+        this.setState({sideDrawerOpen: false});
+    };
+
+    // APP //
+    render() {
+        // Opening and closing Nav bar
+        let sideDrawer;
+        let backdrop;
+        if (this.state.sideDrawerOpen){
+            sideDrawer = <SideDrawer />;
+            backdrop = <Backdrop click={this.backdropClickHandler}/>;
+        }
+
+        return (
+            <div style={{height: '100%'}}>
+                {/* Nav */}
+                <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+                {sideDrawer}
+                {backdrop}
+
+                {/* Content */}
+                <main style={{marginTop: '64px'}}>
+                    <p>This is the page content</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default App;
