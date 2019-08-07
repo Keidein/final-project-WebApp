2117d652162f7f5715ee3105116328a53eee0c3b
rethavanderwalt <42917205+rethavanderwalt@users.noreply.github.com>
2019-08-07 12:01:14 +1200

    Update Solutions.jsx

--- public/Components/Solutions.jsx
+++ public/Components/Solutions.jsx
@@ -49,21 +49,34 @@ export default class Solutions extends React.Component {
                             <p>If your GP rules out a medical cause, then the next step would be to consult with a therapist who has had experience with anxiety disorders. Most Anxiety disorders, in general, are treated with therapy, medication, or a combination of both. </p>
                     </div>
 
-                        <div class="solution-article-triggers">
-                            <h2> How can you avoid anxiety? </h2>
-                            <p>To avoid/reduce the symptoms of anxiety, you can aim to reduce your exposure to some of ots most common triggers:</p>
-                            <ul>
-                                <li>Caffiene</li>
-                                <li>Stress</li>
-                                <li>Skipping meals</li>
-                                <li>Negative thinking</li>
-                                <li>Financial concerns</li>
-                                <li>Conflict</li>
-                                <li>Public events or performances</li>
-                                <li>Medications</li>
-                                <li>Health issues</li>
-                                <li>Persona triggers</li>
-                            </ul>
+                    <div class="solution-article-triggers">
+                        <h2> How can you avoid anxiety? </h2>
+                        <p>To avoid/reduce the symptoms of anxiety, you can aim to reduce your exposure to some of ots most common triggers:</p>
+                        <ul>
+                            <li>Caffiene</li>
+                            <li>Stress</li>
+                            <li>Skipping meals</li>
+                            <li>Negative thinking</li>
+                            <li>Financial concerns</li>
+                            <li>Conflict</li>
+                            <li>Public events or performances</li>
+                            <li>Medications</li>
+                            <li>Health issues</li>
+                            <li>Persona triggers</li>
+                        </ul>
+                    </div>
+                    
+                    <div class="solution-article-helpOthers">
+                        <h2> How can you help others? </h2>
+                        <p>If you know someone who is experiencing Anxiety, you can help them out.</p>
+                        <p>Educate yourself by researching the specific type of anxiety your friend has, its affects, symptoms, and what can be done to help and/or treat it.</p>
+                        <p>Ask the person experiencing Anxiety what kind of support they would prefer, rather than guessing. Tailor your support by observing what works in your particular situation.</p>
+                        <p>Help temper their thinking by making use of a cognitive therapy technique where you ask them the following questions:</p>
+                        <ul>
+                            <li>What’s the worst that could happen?</li>
+                            <li>What’s the best that could happen?</li>
+                            <li>What’s most realistic or likely?</li>
+                        </ul>
                     </div>
                 </main>
            </container> 
