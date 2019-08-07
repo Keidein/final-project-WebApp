3939f6c35fd49dbf4dac548ef71eb53853cc32fb
Ethan-Meleah <40743637+Ethan-Meleah@users.noreply.github.com>
2019-08-08 11:50:59 +1200

    Add files via upload

---
+++ E-Components/App.jsx
@@ -0,0 +1,15 @@
+// Modules.
+import React, { Fragment } from "react";
+
+// Components.
+import Solutions from "./Solutions";
+import Footer from "./Footer";
+
+export default function App() {
+    return(
+        <Fragment>
+            <Solutions/>
+            <Footer/>
+        </Fragment>
+    );
+}
