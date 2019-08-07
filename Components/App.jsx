b37f8608ec80b43cbc442daf0ec0a9bf0008cb93
Ethan-Meleah <40743637+Ethan-Meleah@users.noreply.github.com>
2019-08-08 09:05:00 +1200

    Add files via upload
    ToDo:
    
    - Cleaning up for  About
    - Solutions
    - Footer
    - finding and selecting images for developers

---
+++ Components/App.jsx
@@ -0,0 +1,13 @@
+// Modules.
+import React, { Fragment } from "react";
+
+// Components.
+import About from "./About";
+
+export default function App() {
+    return(
+        <Fragment>
+            <About/>
+        </Fragment>
+    );
+}
