50c1431c2931bc4bf4f72513e22de091fb789f8b
rethavanderwalt <42917205+rethavanderwalt@users.noreply.github.com>
2019-08-08 13:20:37 +1200

    Java + Nav + CSS

--- public/Components/Developers.jsx
+++ public/Components/Developers.jsx
@@ -9,35 +9,28 @@ export default class Developers extends React.Component {
 
     render() {
         return(
-            <contianer>
+            <div class="container">
                 <header>
-                    <img src="https://via.placeholder.com/35" /> 
-                    <h4 class="header-menu-title"> ANXIETYNZ </h4>
-                    <h1 class="header-page-title"> Meet our Team </h1>
+                    <h1 class="header-page-title"> Meet our team! </h1>
                 </header>
 
-                <h2>Developers</h2>
+
                 <div id="first-bio" class="developer-biography">
                     <img src="https://via.placeholder.com/250" />
-                    <h3>Dylan Etheridge</h3>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, fugit. Dolorem, aliquid ipsam assumenda quasi aut ut! Impedit in provident similique hic ea cumque eius neque corrupti. Quas, magni quidem?  </p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae perferendis consectetur ab voluptates similique, tempora repellat, maiores necessitatibus optio voluptatibus totam quaerat ut soluta laudantium dicta? Nam, eligendi minus. </p>
                 </div>
-
-                <br/>
-
                 <div id="second-bio" class="developer-biography">
                     <img src="https://via.placeholder.com/250" />
-                    <h3>Ethan Meleah</h3>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, fugit. Dolorem, aliquid ipsam assumenda quasi aut ut! Impedit in provident similique hic ea cumque eius neque corrupti. Quas, magni quidem?  </p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae perferendis consectetur ab voluptates similique, tempora repellat, maiores necessitatibus optio voluptatibus totam quaerat ut soluta laudantium dicta? Nam, eligendi minus. </p>
                 </div>
-
-                <br/>
-
                 <div id="third-bio" class="developer-biography">
                     <img src="https://via.placeholder.com/250" />
-                    <h3>Retha van der Walt</h3>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, fugit. Dolorem, aliquid ipsam assumenda quasi aut ut! Impedit in provident similique hic ea cumque eius neque corrupti. Quas, magni quidem?  </p>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae perferendis consectetur ab voluptates similique, tempora repellat, maiores necessitatibus optio voluptatibus totam quaerat ut soluta laudantium dicta? Nam, eligendi minus. </p>
                 </div>
-
-                <br/>
-            </contianer>
+            </div>
         )
     }
 }
--- public/Components/Solutions.jsx
+++ public/Components/Solutions.jsx
@@ -9,40 +9,56 @@ export default class Solutions extends React.Component {
 
     render() {
         return(
-           <container>
-                <main style={{marginTop: '64px'}}>
-                    <h2> Self help </h2>
+           <div class="container">
 
-                    <div class="solution-top solution-column solution-left">
-                            <p class="bold">Here are things you can do to;</p>
-                            <ul>
-                                <li>Connect with others</li>
-                                <li>Manage stress</li>
-                                <li>Practise relaxation techniques</li>
-                                <li>Exercise regularly</li>
-                                <li>Get enough sleep</li>
-                                <li>Reduce caffeine and alcohol intake</li>
-                                <li>Quit smoking</li>
-                            </ul>
+
+               <header class="solution-header">
+                    <h1 class="header-page-title"> What can be done? </h1>
+                </header>
+
+               <div id="solution-top-left" class="solution-portion">
+
+                    <div class="solution-inner">
+                       <h2> Self help</h2>
+                       <p class="bold">Here are things you can do to;</p>
+                        <ul>
+                            <li>Connect with others</li>
+                            <li>Manage stress</li>
+                            <li>Practise relaxation techniques</li>
+                            <li>Exercise regularly</li>
+                            <li>Get enough sleep</li>
+                            <li>Reduce caffeine and alcohol intake</li>
+                            <li>Quit smoking</li>
+                        </ul>
                     </div>
 
-                    <div class="solution-top solution-column solution-right">
-                            <img src="https://via.placeholder.com/150"/>
-                            <img src="https://via.placeholder.com/150"/>
-                            <img src="https://via.placeholder.com/150"/>
-                            <img src="https://via.placeholder.com/150"/>
-                            <img src="https://via.placeholder.com/150"/>
-                    </div>
+               </div>
+               <div class="solution-top-right" class="solution-portion">
+                    <img src="./style/images/mind-solutions.png" />
+               </div>
+               
 
-                    <div class="solution-top solution-column solution-left">
-                            <img src="https://via.placeholder.com/150"/>
-                            <img src="https://via.placeholder.com/150"/>
-                            <img src="https://via.placeholder.com/150"/>
-                            <img src="https://via.placeholder.com/150"/>
-                            <img src="https://via.placeholder.com/150"/>
+               <div class="solution-bottom-left" class="solution-portion">
+                <img src="./style/images/body-solutions.jpg" />
+               </div>
+               <div id="solution-bottom-right" class="solution-portion">
+                    <div class="solution-inner">
+                        <h2> How can you help others with anxiety? </h2>
+                        <p>If you know someone who is experiencing Anxiety, you can help them out.</p>
+                        <p>Educate yourself by researching the specific type of anxiety your friend has, its affects, symptoms, and what can be done to help and/or treat it.</p>
+                        <p>Ask the person experiencing Anxiety what kind of support they would prefer, rather than guessing. Tailor your support by observing what works in your particular situation.</p>
+                        <p>Help temper their thinking by making use of a cognitive therapy technique where you ask them the following questions:</p>
+                        <ul>
+                            <li>What’s the worst that could happen?</li>
+                            <li>What’s the best that could happen?</li>
+                            <li>What’s most realistic or likely?</li>
+                        </ul>
                     </div>
+               </div>
 
-                    <div class="solution-top solution-column solution-right">
+
+            {/* EXTRA INFO */}
+               <div class="solution-top solution-column solution-right">
                             <h2> When to seek professional help: </h2>
                             <p>If your anxiety has become so great that it is causing you extreme distress or is disrupting your daily routine, then it is important to seek professional help.</p>
                             <p>If you are experiencing a lot of the physical symptoms, then you should start by visiting your GP to make sure your anxiety isn’t caused by a medical condition. </p>
@@ -65,21 +81,8 @@ export default class Solutions extends React.Component {
                             <li>Persona triggers</li>
                         </ul>
                     </div>
-                    
-                    <div class="solution-article-helpOthers">
-                        <h2> How can you help others? </h2>
-                        <p>If you know someone who is experiencing Anxiety, you can help them out.</p>
-                        <p>Educate yourself by researching the specific type of anxiety your friend has, its affects, symptoms, and what can be done to help and/or treat it.</p>
-                        <p>Ask the person experiencing Anxiety what kind of support they would prefer, rather than guessing. Tailor your support by observing what works in your particular situation.</p>
-                        <p>Help temper their thinking by making use of a cognitive therapy technique where you ask them the following questions:</p>
-                        <ul>
-                            <li>What’s the worst that could happen?</li>
-                            <li>What’s the best that could happen?</li>
-                            <li>What’s most realistic or likely?</li>
-                        </ul>
-                    </div>
-                </main>
-           </container> 
+
+           </div> 
         )
     }
 }
